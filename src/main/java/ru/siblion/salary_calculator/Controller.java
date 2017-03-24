f98cb1249e3aaed4689a0588222aabc86aa8aca8
zhambyl <e.zhambul@gmail.com>
2017-03-24 11:56:57 +0300

    qweqewwe

--- src/main/java/ru/siblion/salary_calculator/Controller.java
+++ src/main/java/ru/siblion/salary_calculator/Controller.java
@@ -1,6 +1,5 @@
 package ru.siblion.salary_calculator;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +14,15 @@ public class Controller {
 
     private final SalaryCalculator calculator = new SalaryCalculator();
 
+    @GetMapping("/")
+    public String hi() {
+        return "hi";
+    }
+
     @GetMapping("/{salary}")
-    public YearReport index(@PathVariable double salary) {
+    public YearReport salary(@PathVariable double salary) {
         return calculator.calculateForYear(salary);
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Controller.class, args);
-    }
+
 }
---
+++ src/main/java/ru/siblion/salary_calculator/Main.java
@@ -0,0 +1,13 @@
+package ru.siblion.salary_calculator;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by zhambyl on 24/03/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Controller.class, args);
+    }
+}
